8edbaf38d2fae431e7a25fdd1c5b3bc98007276f
Eoin Houlihan <ehoulih@tcd.ie>
2016-12-16 19:19:43 +0000

    Main executable uses JWK data from secrets directory

--- auth-service/app/Main.hs
+++ auth-service/app/Main.hs
@@ -2,13 +2,16 @@
 
 module Main where
 
-import Control.Monad.Logger                 (runStdoutLoggingT)
-import Database.Persist.Postgresql
-import Network.Wai.Handler.Warp             (run)
-import Network.Wai.Middleware.RequestLogger (logStdoutDev)
+import           Control.Monad.Logger (runStdoutLoggingT)
+import           Data.Aeson (decodeStrict)
+import qualified Data.ByteString as BS
+import           Database.Persist.Postgresql (createPostgresqlPool, runSqlPool, ConnectionString)
+import           Network.Wai.Handler.Warp (run)
+import           Network.Wai.Middleware.RequestLogger (logStdoutDev)
+import           Servant.Auth.Server (defaultJWTSettings)
 
-import App
-import Models
+import           App (app)
+import           Models (runMigrations)
 
 connStr :: ConnectionString
 connStr = "host=db port=5432 dbname=dfs user=postgres"
@@ -17,4 +20,7 @@ main :: IO ()
 main = do
   pool <- runStdoutLoggingT $ createPostgresqlPool connStr 5
   runSqlPool runMigrations pool
-  run 8080 $ logStdoutDev $ app pool
+  jwkContents <- BS.readFile "/data/secrets/jwk.json"
+  case decodeStrict jwkContents of
+    Nothing -> error "Unable to decode JWK"
+    Just jwk -> run 8080 $ logStdoutDev $ app (defaultJWTSettings jwk) pool
