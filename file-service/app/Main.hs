c0ebb66fc068cf0e20b0f085475cf8eef30ef6b6
Eoin Houlihan <ehoulih@tcd.ie>
2017-01-07 14:54:02 +0000

    Write main for file service

--- file-service/app/Main.hs
+++ file-service/app/Main.hs
@@ -1,6 +1,16 @@
 module Main where
 
-import Lib
+import Control.Monad.Logger (runStdoutLoggingT)
+import Database.Persist.Postgresql (createPostgresqlPool)
+import Network.Wai.Handler.Warp (run)
+import Network.Wai.Middleware.RequestLogger (logStdoutDev)
+
+import App (app)
+import Config (Config(..))
+import Models (connStr)
 
 main :: IO ()
-main = someFunc
+main = do
+  pool <- runStdoutLoggingT $ createPostgresqlPool connStr 5
+  application <- app pool 0
+  run 8080 $ logStdoutDev application
