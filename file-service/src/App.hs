a0daba75023c0cb7e9102b946371431c0a044ad1
Eoin Houlihan <ehoulih@tcd.ie>
2017-01-14 00:09:27 +0000

    Properly implement readFile for file servers

--- file-service/src/App.hs
+++ file-service/src/App.hs
@@ -4,20 +4,23 @@ module App
   ( app
   ) where
 
-import Control.Monad.Except
-import Control.Monad.Reader
-import Data.ByteString.Char8 (pack)
-import Database.Persist.Postgresql
-import GHC.Int (Int64)
-import Network.HTTP.Client (defaultManagerSettings, newManager)
-import Servant
-import Servant.Client
-import Servant.Auth.Client
+import           Control.Monad.Except
+import           Control.Monad.Reader
+import qualified Data.ByteString as BS
+import           Data.ByteString.Base64 (encode)
+import           Data.ByteString.Char8 (pack, unpack)
+import           Database.Persist.Postgresql
+import           GHC.Int (Int64)
+import           Network.HTTP.Client (defaultManagerSettings, newManager)
+import           Servant
+import           Servant.Auth.Client
+import           Servant.Client
+import           System.Directory (doesFileExist)
 
-import AuthAPI (authAPIClient)
-import Config
-import FileAPI
-import Models
+import           AuthAPI (authAPIClient)
+import           Config
+import           FileAPI (fileAPIProxy, FileAPI, HTTPFile(..))
+import           Models
 
 type App = ReaderT Config Handler
 
@@ -37,14 +40,17 @@ readFileImpl :: Maybe String -> String -> App HTTPFile
 readFileImpl maybeToken path =
   case maybeToken of
     Nothing -> throwError err401 { errBody = "No authentication token provided" }
-    Just token -> do
-      manager <- asks manager
-      authBase <- asks authBase
-      res <- liftIO $ runExceptT $ verifyJWT (Token $ pack token) manager authBase
-      case res of
-        Left _ -> throwError err401 { errBody = "Invalid authentication token" }
-        Right _ -> pure NoContent
+    Just token -> authenticate (Token $ pack token) $ do
+      let filePath = "/data/files/" ++ path
+      exists <- liftIO $ doesFileExist filePath
+      if exists
+      then do
+        fileContents <- liftIO $ BS.readFile filePath
+        pure HTTPFile { path = path, contents = unpack $ encode fileContents }
+      else throwError err404
 
+
+writeFileImpl :: Maybe String -> HTTPFile -> App NoContent
 writeFileImpl = undefined
 
 authenticate :: Token -> App a -> App a
