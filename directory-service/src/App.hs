ed2a7dcbf4ad704a634b032f907b126257955e3a
Eoin Houlihan <ehoulih@tcd.ie>
2017-01-21 20:12:07 +0000

    Implement file listing for directory service

--- directory-service/src/App.hs
+++ directory-service/src/App.hs
@@ -1,18 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module App
   ( app
   ) where
 
-import Control.Monad.Reader (ReaderT)
+import Control.Monad.Except (runExceptT)
+import Control.Monad.Reader (asks, liftIO, ReaderT)
+import Data.ByteString.Char8 (pack)
 import Database.Persist.Postgresql
 import Network.HTTP.Client (defaultManagerSettings, newManager)
 import Servant
-import Servant.Auth.Client
+import Servant.Auth.Client (Token(..))
 import Servant.Client
 
 import AuthAPI (authAPIClient)
 import Config (Config(..))
 import DirectoryAPI (directoryAPIProxy, DirectoryAPI)
-import Models
+import Models (runDB, File, Node)
 
 type App = ReaderT Config Handler
 
@@ -29,7 +33,12 @@ server :: ServerT DirectoryAPI App
 server = ls :<|> whereis :<|> registerFileServer
 
 ls :: Maybe String -> App [File]
-ls = undefined
+ls maybeToken =
+  case maybeToken of
+    Nothing -> throwError err401 { errBody = "No authentication token provided" }
+    Just token -> authenticate (Token $ pack token) $ do
+      allFiles <- runDB $ selectList [] []
+      pure $ entityVal <$> allFiles
 
 whereis :: Maybe String -> App Node
 whereis = undefined
@@ -37,4 +46,15 @@ whereis = undefined
 registerFileServer :: App NoContent
 registerFileServer = undefined
 
+-- General authentication function, performs action only when succesfully authenticated
+authenticate :: Token -> App a -> App a
+authenticate token action = do
+  manager <- asks manager
+  authBase <- asks authBase
+  res <- liftIO $ runExceptT $ verifyJWT token manager authBase
+  case res of
+    Left _ -> throwError err401 { errBody = "Invalid authentication token" }
+    Right _ -> action
+
+-- Pattern match out the routes of the AuthAPI
 _ :<|> _ :<|> verifyJWT = authAPIClient
